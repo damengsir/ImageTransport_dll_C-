304a7a99d4da7a41841d08001634506d9dd31dd8
Darmon <xiaowangyx1@163.com>
2020-07-21 17:04:37 +0800

    Add: GUI of WinForm

---
+++ ImageTransGUI/ImageTransGUI/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ImageTransGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadImage = new System.Windows.Forms.Button();
+            this.pictureBoxReturn = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRead = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxReturn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReadImage
+            // 
+            this.ReadImage.Location = new System.Drawing.Point(62, 36);
+            this.ReadImage.Name = "ReadImage";
+            this.ReadImage.Size = new System.Drawing.Size(75, 23);
+            this.ReadImage.TabIndex = 0;
+            this.ReadImage.Text = "读取图像";
+            this.ReadImage.UseVisualStyleBackColor = true;
+            this.ReadImage.Click += new System.EventHandler(this.ReadImage_Click);
+            // 
+            // pictureBoxReturn
+            // 
+            this.pictureBoxReturn.Location = new System.Drawing.Point(633, 94);
+            this.pictureBoxReturn.Name = "pictureBoxReturn";
+            this.pictureBoxReturn.Size = new System.Drawing.Size(576, 526);
+            this.pictureBoxReturn.TabIndex = 1;
+            this.pictureBoxReturn.TabStop = false;
+            // 
+            // pictureBoxRead
+            // 
+            this.pictureBoxRead.Location = new System.Drawing.Point(75, 94);
+            this.pictureBoxRead.Name = "pictureBoxRead";
+            this.pictureBoxRead.Size = new System.Drawing.Size(520, 526);
+            this.pictureBoxRead.TabIndex = 2;
+            this.pictureBoxRead.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 768);
+            this.Controls.Add(this.pictureBoxRead);
+            this.Controls.Add(this.pictureBoxReturn);
+            this.Controls.Add(this.ReadImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxReturn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRead)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadImage;
+        private System.Windows.Forms.PictureBox pictureBoxReturn;
+        private System.Windows.Forms.PictureBox pictureBoxRead;
+    }
+}
+
---
+++ ImageTransGUI/ImageTransGUI/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace ImageTransGUI
+{
+    public partial class Form1 : Form
+    {
+
+#if DEBUG
+        const string DllName = "ImageTransd.dll";
+#else
+        const string DllName = "ImageTrans.dll";
+#endif
+
+        [DllImport(DllName, EntryPoint = "ImageProcess", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ImageProcess(IntPtr img, int width, int height, int step);
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void ReadImage_Click(object sender, EventArgs e)
+        {
+            //读取图像方法一
+            //{
+            //    pictureBoxRead.SizeMode = PictureBoxSizeMode.StretchImage;
+            //    Image myImage = Image.FromFile(@"D:/1.jpg");
+            //    pictureBoxRead.Image = myImage;
+            //}
+
+            {
+                //1、Bitmap图像读取,并显示在控件上
+                Bitmap bmp = new Bitmap("D:/UVSS1.jpg");
+                Image img = Image.FromHbitmap(bmp.GetHbitmap());
+                pictureBoxRead.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBoxRead.Image = img;
+
+
+                //2、锁定内存变成指针形式
+                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+          
+                //3、调用Dll处理
+                Form1.ImageProcess(bitmapData.Scan0, bitmapData.Width, bitmapData.Height, bitmapData.Stride);
+
+                //4、//解除锁定内存
+                bmp.UnlockBits(bitmapData);
+                
+
+                //5、显示处理后的图像
+                Image Img2 = Image.FromHbitmap(bmp.GetHbitmap());
+                pictureBoxReturn.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBoxReturn.Image = Img2;
+
+            }
+            
+        }
+
+
+    }
+}
